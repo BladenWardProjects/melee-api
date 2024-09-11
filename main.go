6f47887ad668d0f59fe3d0a1613afca7fb83d4fc
Bladen Ward <bladen@bladen.dev>
2024-09-10 22:52:03 -0500

    feat: WIP import files yaml -> go

--- main.go
+++ main.go
@@ -6,21 +6,22 @@ import (
 	"log"
 
 	"github.com/BladenWard/melee-api/db"
+	"github.com/BladenWard/melee-api/seed"
 	"github.com/BladenWard/melee-api/server"
 )
 
 func main() {
 	listenAddr := flag.String("listen", ":3030", "Listen address")
-	// seed := flag.Bool("seed", false, "Seed the database")
+	seedFlag := flag.Bool("seed", false, "Seed the database")
 	flag.Parse()
 
 	server := server.NewServer(*listenAddr)
 
 	db.Init()
-	// if *seed {
-	// TODO: Seed the database
-	// s.Seed()
-	// }
+	if *seedFlag {
+		// TODO: Seed the database
+		seed.Seed()
+	}
 
 	fmt.Println("Listening on", server.ListenAddr)
 	log.Fatal(server.Start())
---
+++ seed/seed.go
@@ -0,0 +1,39 @@
+package seed
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+
+	"github.com/BladenWard/melee-api/types"
+)
+
+func Seed() {
+	fmt.Println("Seeding database...")
+	characterFile, _ := os.Open("seed/characters/fox.yaml")
+	defer characterFile.Close()
+
+	characterYaml := make([]byte, 10000)
+	count, error := characterFile.Read(characterYaml)
+	characterYaml = characterYaml[:count]
+	if error != nil {
+		log.Fatalf("error: %v", error)
+	}
+
+	// TODO: Import the yaml file
+	// fmt.Println(string(characterYaml))
+
+	characterSchema := types.Character{}
+	err := yaml.Unmarshal(characterYaml, &characterSchema)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	d, err := yaml.Marshal(&characterSchema)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(d))
+	// fmt.Println(string(yamlFile))
+}
