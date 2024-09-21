d1622f3fdd019680773f3a73e6d8b6f0ccb048dd
Bladen Ward <bladen@bladen.dev>
2024-09-21 12:33:28 -0500

    fix: moved away from yaml man that sucks

--- types/character.go
+++ types/character.go
@@ -40,91 +40,91 @@ func (ym *YamlMap) Value(ctx context.Context, field *schema.Field, dst reflect.V
 
 // TODO: Fix script, move the damage array into 2 fields
 type Character struct {
-	Name           string         `yaml:"name"`
-	ID             uint           `yaml:"id" gorm:"primaryKey,autoIncrement"`
-	Weight         uint           `yaml:"weight"`
-	FastfallSpeed  float64        `yaml:"fastfall_speed"`
-	DashSpeed      float64        `yaml:"dash_speed"`
-	RunSpeed       float64        `yaml:"run_speed"`
-	WavedashLength uint           `yaml:"wavedash_length_rank"`
-	Galint         uint           `yaml:"galint"`
-	JumpSquat      uint           `yaml:"jump_squat"`
-	Walljump       bool           `yaml:"walljump"`
-	GroundAttacks  []GroundAttack `yaml:"ground"`
-	Aerials        []Aerial       `yaml:"aerial"`
-	Specials       []Special      `yaml:"special"`
-	Grabs          []Grab         `yaml:"grab"`
-	Throws         []Throw        `yaml:"throw"`
-	Dodges         []Dodge        `yaml:"dodge"`
+	Name           string         `json:"name"`
+	ID             uint           `json:"id" gorm:"primaryKey,autoIncrement"`
+	Weight         uint           `json:"weight"`
+	FastfallSpeed  float64        `json:"fastfall_speed"`
+	DashSpeed      float64        `json:"dash_speed"`
+	RunSpeed       float64        `json:"run_speed"`
+	WavedashLength uint           `json:"wavedash_length_rank"`
+	Galint         uint           `json:"galint"`
+	JumpSquat      uint           `json:"jump_squat"`
+	Walljump       bool           `json:"walljump"`
+	GroundAttacks  []GroundAttack `json:"ground" gorm:"serializer:json"`
+	Aerials        []Aerial       `json:"aerial" gorm:"serializer:json"`
+	Specials       []Special      `json:"special" gorm:"serializer:json"`
+	Grabs          []Grab         `json:"grab" gorm:"serializer:json"`
+	Throws         []Throw        `json:"throw" gorm:"serializer:json"`
+	Dodges         []Dodge        `json:"dodge" gorm:"serializer:json"`
 }
 
 type GroundAttack struct {
-	ID          uint    `yaml:"id" gorm:"primaryKey"`
-	Name        string  `yaml:"name"`
-	Start       uint    `yaml:"start"`
-	End         uint    `yaml:"end"`
-	TotalFrames uint    `yaml:"total_frames"`
-	IasaFrames  *uint   `yaml:"iasa_frames"`
-	ShieldStun  uint    `yaml:"shield_stun"`
-	BaseDamage  uint32  `yaml:"base_damage"`
-	WeakDamage  *uint32 `yaml:"weak_damage"`
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	Name        string `json:"name"`
+	Start       uint   `json:"start"`
+	End         uint   `json:"end"`
+	TotalFrames uint   `json:"frames"`
+	IasaFrames  *uint  `json:"iasa_frames"`
+	ShieldStun  uint   `json:"shield_stun"`
+	BaseDamage  uint   `json:"base_damage"`
+	WeakDamage  *uint  `json:"weak_damage"`
 	CharacterID uint
 }
 
 type Aerial struct {
-	ID          uint    `yaml:"id" gorm:"primaryKey"`
-	Name        string  `yaml:"name"`
-	Start       uint    `yaml:"start"`
-	End         uint    `yaml:"end"`
-	TotalFrames uint    `yaml:"total_frames"`
-	ShieldStun  uint    `yaml:"shield_stun"`
-	BaseDamage  uint32  `yaml:"base_damage"`
-	WeakDamage  *uint32 `yaml:"weak_damage"`
-	LandingLag  uint    `yaml:"landing_lag"`
-	LCancelLag  uint    `yaml:"l_cancel_lag"`
-	AutoCancel  *int    `yaml:"auto_cancel"`
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	Name        string `json:"name"`
+	Start       uint   `json:"start"`
+	End         uint   `json:"end"`
+	TotalFrames uint   `json:"frames"`
+	ShieldStun  uint   `json:"shield_stun"`
+	BaseDamage  uint   `json:"base_damage"`
+	WeakDamage  *uint  `json:"weak_damage"`
+	LandingLag  uint   `json:"landing_lag"`
+	LCancelLag  uint   `json:"l_cancel_lag"`
+	AutoCancel  *int   `json:"auto_cancel"`
 	CharacterID uint
 }
 
 type Special struct {
-	ID                 uint    `yaml:"id" gorm:"primaryKey"`
-	Name               string  `yaml:"name"`
-	Start              uint    `yaml:"start"`
-	End                uint    `yaml:"end"`
-	TotalFrames        uint    `yaml:"total_frames"`
-	ShieldStun         *uint   `yaml:"shield_stun"`
-	BaseDamage         uint32  `yaml:"base_damage"`
-	WeakDamage         *uint32 `yaml:"weak_damage"`
-	LandingLag         *uint   `yaml:"landing_lag"`
-	LandingFallSpecial *uint   `yaml:"landing_fall_special"`
+	ID                 uint   `json:"id" gorm:"primaryKey"`
+	Name               string `json:"name"`
+	Start              uint   `json:"start"`
+	End                uint   `json:"end"`
+	TotalFrames        uint   `json:"frames"`
+	ShieldStun         *uint  `json:"shield_stun"`
+	BaseDamage         uint   `json:"base_damage"`
+	WeakDamage         *uint  `json:"weak_damage"`
+	LandingLag         *uint  `json:"landing_lag"`
+	LandingFallSpecial *uint  `json:"landing_fall_special"`
 	CharacterID        uint
 }
 
 type Grab struct {
-	ID          uint   `yaml:"id" gorm:"primaryKey"`
-	Name        string `yaml:"name"`
-	Start       uint   `yaml:"start"`
-	TotalFrames uint   `yaml:"total_frames"`
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	Name        string `json:"name"`
+	Start       uint   `json:"start"`
+	TotalFrames uint   `json:"frames"`
 	CharacterID uint
 }
 
 type Throw struct {
-	ID          uint    `yaml:"id" gorm:"primaryKey"`
-	Name        string  `yaml:"name"`
-	Start       uint    `yaml:"start"`
-	End         uint    `yaml:"end"`
-	TotalFrames uint    `yaml:"total_frames"`
-	BaseDamage  uint32  `yaml:"base_damage"`
-	WeakDamage  *uint32 `yaml:"weak_damage"`
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	Name        string `json:"name"`
+	Start       uint   `json:"start"`
+	End         uint   `json:"end"`
+	TotalFrames uint   `json:"frames"`
+	BaseDamage  uint   `json:"base_damage"`
+	WeakDamage  *uint  `json:"weak_damage"`
 	CharacterID uint
 }
 
 type Dodge struct {
-	ID                 uint   `yaml:"id" gorm:"primaryKey"`
-	Name               string `yaml:"name"`
-	Start              uint   `yaml:"start"`
-	End                uint   `yaml:"end"`
-	TotalFrames        uint   `yaml:"total_frames"`
-	LandingFallSpecial *uint  `yaml:"landing_fall_special"`
+	ID                 uint   `json:"id" gorm:"primaryKey"`
+	Name               string `json:"name"`
+	Start              uint   `json:"start"`
+	End                uint   `json:"end"`
+	TotalFrames        uint   `json:"frames"`
+	LandingFallSpecial *uint  `json:"landing_fall_special"`
 	CharacterID        uint
 }
