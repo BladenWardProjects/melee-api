502684f0ba01f94f43c5d4a3e26b69a9f874bef0
Bladen Ward <bladen@bladen.dev>
2024-09-24 14:14:03 -0500

    fix: added bowser to filelist

--- seed/characters/seed-characters.sh
+++ seed/characters/seed-characters.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 CHARACTERS=(
-    "captain_falcon" "donkey_kong" "dr._mario" "falco" "fox" "ganondorf"
+    "bowser" "captain_falcon" "donkey_kong" "dr._mario" "falco" "fox" "ganondorf"
     "ice_climbers" "kirby" "bowser" "link" "luigi" "mario" "marth"
     "mewtwo" "mr._game_&_watch" "ness" "peach" "pichu" "pikachu"
     "jigglypuff" "roy" "samus" "sheik" "yoshi" "young_link" "zelda"
 )
-
-## BUG: MAYBE: The character name at the top of the file might be throwing the
-##      Yaml parser off.
 
 for CHARACTER in "${CHARACTERS[@]}"
 do
