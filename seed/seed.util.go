aea20ba5f2ecebc7559e3e235d59762d8b299aee
Bladen Ward <bladen@bladen.dev>
2024-09-24 14:42:19 -0500

    refactor: moved DROP_TABLES into util

---
+++ seed/seed.util.go
@@ -0,0 +1,30 @@
+package seed
+
+import (
+	"github.com/BladenWard/melee-api/db"
+	"github.com/BladenWard/melee-api/types"
+)
+
+func DROP_TABLES(db *db.DB) {
+	err := db.DB.Migrator().DropTable(
+		&types.Character{},
+		&types.GroundAttack{},
+		&types.Aerial{},
+		&types.Special{},
+		&types.Grab{},
+		&types.Throw{},
+		&types.Dodge{},
+	)
+	if err != nil {
+		panic("failed to drop tables: " + err.Error())
+	}
+	db.DB.AutoMigrate(
+		&types.Character{},
+		&types.GroundAttack{},
+		&types.Aerial{},
+		&types.Special{},
+		&types.Grab{},
+		&types.Throw{},
+		&types.Dodge{},
+	)
+}
