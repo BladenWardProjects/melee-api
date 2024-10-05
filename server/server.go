b0627a596c03244af12ba82556a2d3051bbf4efa
Bladen Ward <bladen@bladen.dev>
2024-10-05 18:12:16 -0500

    refactor: move api into separate module

--- server/server.go
+++ server/server.go
@@ -1,8 +1,6 @@
 package server
 
 import (
-	"strconv"
-
 	"github.com/BladenWard/melee-api/db"
 
 	"github.com/labstack/echo/v4"
@@ -57,81 +55,4 @@ func makeJsonRespFromId(id uint, c echo.Context, db db.Storage) error {
 		return c.JSON(404, err)
 	}
 	return c.JSON(200, character)
-}
-
-func (s *Server) NewApi(e *echo.Echo) {
-	char := e.Group("/character")
-	{
-		char.GET("", func(c echo.Context) error {
-			characters, err := s.store.GetCharacters()
-			if err != nil {
-				return c.JSON(404, err)
-			}
-			return c.JSON(200, characters)
-		})
-
-		char.GET("/", func(c echo.Context) error {
-			characters, err := s.store.GetCharacters()
-			if err != nil {
-				return c.JSON(404, err)
-			}
-			return c.JSON(200, characters)
-		})
-
-		char.GET("/:id", func(c echo.Context) error {
-			id, err := strconv.Atoi(c.Param("id"))
-			if err != nil {
-				return makeJsonRespFromName(c.Param("id"), c, s.store)
-			} else {
-				return makeJsonRespFromId(uint(id), c, s.store)
-			}
-		})
-	}
-
-	song := e.Group("/song")
-	{
-		song.GET("", func(c echo.Context) error {
-			songs, err := s.store.GetSongs()
-			if err != nil {
-				return c.JSON(404, err)
-			}
-			return c.JSON(200, songs)
-		})
-
-		song.GET("/", func(c echo.Context) error {
-			songs, err := s.store.GetSongs()
-			if err != nil {
-				return c.JSON(404, err)
-			}
-			return c.JSON(200, songs)
-		})
-
-		song.GET("/:id", func(c echo.Context) error {
-			id, err := strconv.Atoi(c.Param("id"))
-			if err != nil {
-				return c.JSON(404, err)
-			}
-
-			song, err := s.store.GetSongByID(uint(id))
-			if err != nil {
-				return c.JSON(404, err)
-			}
-			return c.JSON(200, song)
-		})
-	}
-
-	stage := e.Group("/stage")
-	{
-		stage.GET("", func(c echo.Context) error {
-			return c.String(200, "All stages")
-		})
-
-		stage.GET("/", func(c echo.Context) error {
-			return c.String(200, "All stages")
-		})
-
-		stage.GET("/:id", func(c echo.Context) error {
-			return c.String(200, "Stage: "+c.Param("id"))
-		})
-	}
 }
